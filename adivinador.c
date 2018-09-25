d30b383dc531a4fa026718c55e68039ab044e701
Oliver ALR <dcheroes6@gmail.com>
2018-09-25 00:41:50 -0500

    deployment

--- adivinador.c
+++ adivinador.c
@@ -7,57 +7,44 @@
 #define FALSE 0
 
 char array(char s[]); //Prototipo.
+void core();
 void gotoxy(int x, int y);
 
 void gotoxy(int x, int y){
   printf("%c[%d;%df",0x1B,y,x); //Dirección a la que apunta gotoxy() en la terminal.
 }
 
+
+//Estructura principal del código.
 int main(void){
+  char k;
+  int col;
 
-  //Arreglos uno con el mensaje a pantalla y otro vacío.
-  char inv[MAX] = {"Oh gran Huitzilopochtli dime: "}, save[MAX];
-  int c,d, i = -1, j = 0, col = 0;
-
-  gotoxy(1,1); for(col=0; col < 70; col++) printf(" ");
+  //Estos gotoxy limpian la barra de comandos, dejando la terminal limpia.
+  gotoxy(1,1); for(col=0; col < 100; col++) printf(" ");
   printf("\n");
-  gotoxy(2,2); for(col=0; col < 70; col++) printf(" ");
+  gotoxy(2,2); for(col=0; col < 100; col++) printf(" ");
   printf("\n");
 
   system("/bin/stty raw");
-  c = getchar();
-  if(c == '1'){
-    system("/bin/stty raw"); //Ejecución de System().
-    while((c=getchar())!= '.') {
-      i += 1;
-      save[i] = c;    //Recolleción de caracteres a un arreglo.
-      printf("%c",inv[i]);
+  char r = getchar();
+  if(r == '1'){
+    core();
+
+  }else{
+
+    do{
+      printf("Tu no eres digno de hablar con Huitzilopochtli >:V\n\r\n");
+      system("/bin/stty raw");
+      k=getchar();
+
+    }while(k != '2');
+
+    core();
   }
 
-    system("/bin/stty raw");
-    while((d=getchar())!= '.') { //texto de "humo".
-      printf("%c", d);
-  }
-
-    printf("\t\t");
-    array(save); //Llamada y parametrización.
-    printf("\n\n");
-
-    system("/bin/stty cooked"); 
-    printf("\n");
-  }
-
-  else {
-  
-    printf("Tu no eres digno de hablar con Huitzilopochtli >:V\n");
-    system("/bin/stty cooked");
-    printf("\n");
-  }
-
-  //system("/bin/stty cooked");
-  
-  return 0; 
-
+  printf("\n");
+  return 0;
 }
 
 //Impresión Básica de un Arreglo.
@@ -68,4 +55,36 @@ char array(char s[]){
     printf("%c",s[k]);
   }
   return TRUE;
+}
+
+//Funcionamiento Principal
+void core()
+{
+  //Arreglos uno con el mensaje a pantalla y otro vacío.
+  char inv[MAX] = {"Oh gran Huitzilopochtli dime: "}, save[MAX];
+  int c,d, i = -1, j = 0;
+  char q[] = {"\U0001F61C \U0001F61C \U0001F61C"};
+
+  system("/bin/stty raw"); //Ejecución de System().
+  while((c=getchar())!= '.') {
+    i += 1;
+    save[i] = c;    //Recolleción de caracteres a un arreglo.
+    printf("%c",inv[i]);
+  }
+
+  system("/bin/stty raw");
+  while((d=getchar())!= '.') { //texto de "humo".
+    printf("%c", d);
+  }
+
+  printf("\n\n\r");
+  array(save); //Llamada y parametrización.
+  int l, large = strlen(q);
+  for(l = 0; l < large; l++){ //Impresión del Emoji.
+    printf("%c", q[l]);
+  }
+  printf("\n\n");
+
+  system("/bin/stty cooked");
+  printf("\n\r");
 }
