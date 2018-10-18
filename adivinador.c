29963c933a4bea7715c610a44b50b2fb33d4a255
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2018-10-18 09:04:41 -0500

    Add files via upload

--- adivinador.c
+++ adivinador.c
@@ -1,112 +1,112 @@
 #include <stdio.h>
-#include <stdlib.h> //Biblioteca para system()
+#include <stdlib.h> //Librería para system()
 #include <string.h>
-#include <unistd.h> // Si, es parte del ANSI.
-#include <stdbool.h> // ANSI C99 para definir booleanos
+
 
 #define MAX 1000
-
-//uso redundante de booleanos
-//lo usaré para explicar como funcionan en C
-bool verdadero = true;
-bool falso = false;
+#define TRUE 1
+#define FALSE 0
 
 char array(char s[]); //Prototipo.
 void core();
 void gotoxy(int x, int y);
 
-//Dirección a la que apunta gotoxy() en la terminal.
-void gotoxy(int x, int y)
-{
-        printf("%c[%d;%df",0x1B,y,x);
+void gotoxy(int x, int y){
+  printf("%c[%d;%df",0x1B,y,x); //Dirección a la que apunta gotoxy() en la terminal.
 }
-
-/* Esta función funcionará en terminales ANSI que además respeten el
- * estándar POSIX, funcionará en todos los sitemas operativos ya que
- * ANSI soporta las secuencias de escape*/
-
-void clear_screen()
-{
-        const char *clear_screen_ansi = "\e[1;1H\e[2J";
-        write(STDOUT_FILENO, clear_screen_ansi, 12);
-}
-
 
 //Estructura principal del código.
-int main()
-{
-        char k;
+int main(void){
+  char k,ex;
+  int col;
 
-        clear_screen();
-        // printf("\e[1;1H\e[2J");
+    //Estos gotoxy limpian la barra de comandos, dejando la terminal limpia.
+    gotoxy(1,1); for(col=0; col < 110; col++) printf(" ");
+    printf("\n");
+    gotoxy(2,2); for(col=0; col < 110; col++) printf(" ");
+    printf("\n");
 
-        system("/bin/stty raw");
+    do{
 
-        char r = getchar();
-        if(r == '1') {
-                core();
+       system("/bin/stty raw");
+       char r = getchar();
+       if(r == '1'){
+          core();
+
         }else{
-                //No más de 3 niveles de sangría, de lo contrario el
-                //código está mal.
-                do{
-                        // Corte del mensaje, para respetar la terminal
-                        // ANSI de 80 x 24.
-                        printf("No eres digno de hablar con\
-                                Huitzilopochtli >:V\n\r\n");
 
-                        system("/bin/stty raw");
-                        k=getchar();
+        do{
+          
+          printf("Tu no eres digno de hablar con Huitzilopochtli >:V\n\r\n");
+          system("/bin/stty raw");
+          k=getchar();
 
-                }while(k != '2');
-                core();
+        }while(k != '2');
+
+          core();
         }
-        printf("\n");
 
-return 0;
+      system("/bin/stty raw");
+
+    } while((ex=getchar()) == '.');
+
+    printf("\n");
+    return 0;
 }
 
 //Impresión Básica de un Arreglo.
 char array(char s[]){
-        int k, len = strlen(s);
+  int k, len = strlen(s);
 
-        for(k = 0; k < len; k++){
-                printf("%c",s[k]);
-        }
-  return verdadero;
+    for(k = 0; k < len; k++){
+      printf("%c",s[k]);
+    }
+    return TRUE;
 }
 
 //Funcionamiento Principal
 void core()
 {
-        //Arreglos uno con el mensaje a pantalla y otro vacío.
-        char inv[MAX] = {"Oh gran Huitzilopochtli dime: "}, save[MAX];
-        int c,d, i = -1;
-        char q[] = {"\U0001F61C \U0001F61C \U0001F61C"};
+  //Arreglos uno con el mensaje a pantalla y otro vacío.
+  char inv[MAX] = {"Oh gran Huitzilopochtli dime: "}, save[MAX], a[MAX];
+  int c,d, i = -1, j = 0, p = 0, w = 0, ty = strlen(a);
+  char q[] = {"\U0001F61C \U0001F61C \U0001F61C"}, empty;
 
-        system("/bin/stty raw"); //Ejecución de System().
+  system("/bin/stty raw"); //Ejecución de System().
+  while((c=getchar())!= '.') {
+      i += 1;
+      save[i] = c;    //Recolección de caracteres a un arreglo.
+      printf("%c",inv[i]);
+  }
 
-        while((c=getchar())!= '.') {
-                i += 1;
-                save[i] = c;    //Recolección de caracteres a un arreglo.
-                printf("%c",inv[i]);
-        }
+  system("/bin/stty raw");
+  while((d=getchar())!= '.') { //texto de "humo".
+      printf("%c", d);
+      a[p] = d;
+    
+      //Fase de Borrado con BackSpace.
+      if(d == '\b'){
+        do{
+          
+         p--;
+         a[p] = ' ';
+         printf("%c",a[p]); 
+        } while(d == '\b');
+      }
+      p++;
+  }
 
-        system("/bin/stty raw");
+  printf("\n\n\r");
+  
+  array(save); //Llamada y parametrización.
+  
+  int l, large = strlen(q);
+    for(l = 0; l < large; l++){ //Impresión del Emoji.
+      printf("%c", q[l]);
+    }
+    printf("\n\n");
 
-        while((d=getchar())!= '.') { //texto de "humo".
-                printf("%c", d);
-        }
-
-        printf("\n\n\r");
-        array(save); //Llamada y parametrización.
-        int l, large = strlen(q);
-
-        for(l = 0; l < large; l++){ //Impresión del Emoji.
-                printf("%c", q[l]);
-        }
-
-        printf("\n\n");
-
-        system("/bin/stty cooked");
-        printf("\n\r");
+  system("/bin/stty cooked");
+  printf("\n\r");
+  
 }
